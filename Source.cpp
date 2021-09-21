77b69dd6f426dbba40709bbbfd4a758224ac951d
Mateusz Marek <254959@student.pwr.edu.pl>
2021-09-21 19:48:11 +0200

    e

--- Source.cpp
+++ Source.cpp
@@ -5,51 +5,6 @@
 #define MyWindowTest
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
-	/*//Create a class
-	const wchar_t className[] = L"test";
-	WNDCLASS myClass = {};
-	myClass.lpfnWndProc = WindowProc;
-	myClass.hInstance = hInstance;
-	myClass.lpszClassName = className;
-	
-	myClass.style = CS_OWNDC;
-	myClass.lpszMenuName = NULL;
-
-	//Register it
-	RegisterClass(&myClass);
-
-	StateInfo* pState = new (std::nothrow) StateInfo;
-
-	if (pState == NULL)
-	{
-		return 0;
-	}
-
-	//Create instance
-	HWND hwnd = CreateWindowEx(
-		0,
-		className,
-		L"Thats a window name",
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-		NULL,
-		NULL,
-		hInstance,
-		pState
-	);
-
-	if (!hwnd)
-		return -1;
-
-	ShowWindow(hwnd, nCmdShow);
-	MSG msg;
-	while (GetMessage(&msg, nullptr, 0, 0) > 0) {
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
-	//while (true);
-
-	//delete pState;*/
 
 	HRESULT result = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
 
